149adac253725e28e228db4dd1134529b74291d4
gangwarady <103820233+gangwarady@users.noreply.github.com>
2022-05-27 10:57:32 +0530

    Create Merge_intervals.cpp

---
+++ Merge_intervals.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        vector<vector<int>> v;
+        sort(intervals.begin(), intervals.end());
+        for(int i =0; i< intervals.size(); i++){
+            if(v.empty()){
+                v.push_back(intervals[i]);
+            }
+            else{
+                if(v.back()[1]>=intervals[i][0]){
+                    v.back()[1] = max(v.back()[1],intervals[i][1] );
+                }
+                else{
+                    v.push_back(intervals[i]);
+                }
+            }
+        }
+        return v;
+    }
+};
