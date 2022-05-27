943d028d3036819263c1680f7edc09cf3063e982
gangwarady <103820233+gangwarady@users.noreply.github.com>
2022-05-27 10:55:49 +0530

    Update RotateMatrix.cpp

--- RotateMatrix.cpp
+++ RotateMatrix.cpp
@@ -1,6 +1,7 @@
-class Solution {
-public:
-    void rotate(vector<vector<int>>& matrix) {
+#include<bits/stdc++.h>
+
+using namespace std;
+ void rotate(vector<vector<int>>& matrix) {
         //cout<<matrix.size();
         for(int i =0; i<matrix.size(); i++){
             for(int j = 0; j<i; j++){
@@ -11,4 +12,16 @@ public:
             reverse(matrix[i].begin(), matrix[i].end());
         }
     }
-};
+int main() {
+  vector < vector < int >> arr;
+  arr =  {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+  rotate(arr);
+  cout << "Rotated Image" << endl;
+  for (int i = 0; i < arr.size(); i++) {
+    for (int j = 0; j < arr[0].size(); j++) {
+      cout << arr[i][j] << " ";
+    }
+    cout << "\n";
+  }
+
+}
